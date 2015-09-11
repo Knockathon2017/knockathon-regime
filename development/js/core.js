4ed9b87e8ade174e8e835433b62f23bc37f6a0ef
Akshay Sharma <akshays@exzeo.com>
2015-09-11 21:29:55 +0530

    basic structure up

---
+++ development/js/core.js
@@ -0,0 +1,9 @@
+ var sparkApp = angular.module('spark', []);
+
+alert("akshay");
+    sparkApp.controller('sparkController', function ($scope, $http) {
+       
+                    $scope.data = {
+						name:"Akshay"
+					};
+    });
