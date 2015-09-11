8a79c3d69ac80e7f0edaa6ce55403535118130d0
Akshay Sharma <akshays@exzeo.com>
2015-09-11 22:50:03 +0530

    login button click function done

--- development/js/core.js
+++
@@ -1,9 +0,0 @@
- var sparkApp = angular.module('spark', []);
-
-alert("akshay");
-    sparkApp.controller('sparkController', function ($scope, $http) {
-       
-                    $scope.data = {
-						name:"Akshay"
-					};
-    });
