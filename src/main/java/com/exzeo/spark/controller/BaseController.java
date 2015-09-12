9b0f964b6bc52f614e699e6314b98218f912d785
dhruvr <dhruvr@exzeo.com>
2015-09-12 05:43:18 +0530

    -Cors filter improve

--- src/main/java/com/exzeo/spark/controller/BaseController.java
+++ src/main/java/com/exzeo/spark/controller/BaseController.java
@@ -2,22 +2,16 @@ package com.exzeo.spark.controller;
 
 import static com.exzeo.spark.common.Constant.SUCCESS_RESPONSE;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.exzeo.spark.model.User;
 
 @RestController
 public class BaseController {
 
-	@RequestMapping(value="/signup", method=RequestMethod.POST)
+	@RequestMapping(value="/signup", method={RequestMethod.GET})
 	public @ResponseBody String signUp(@RequestBody User user) {
-		
-		
-		return SUCCESS_RESPONSE;
+        return SUCCESS_RESPONSE;
 	}
 	
 	
